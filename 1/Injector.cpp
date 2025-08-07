0e323619fa155b6aa8712086a5e0f72352717941
shuga <shugarok1@gmail.com>
2025-08-07 21:36:36 +0200

    +better logs +NtCreateThreadEx with fallback

--- 1/Injector.cpp
+++ 1/Injector.cpp
@@ -1,140 +1,165 @@
 #include "main.h"
 
+
+
 wchar_t dllPath[] = L"C:\\prog\\C++\\learn\\1\\1\\Injector.dll";
 unsigned int dllSiz = static_cast<unsigned int>((wcslen(dllPath) + 1) * sizeof(wchar_t));
 
 
-int injectorMain(int pid) {
-
-    typedef NTSTATUS(NTAPI* pNtCreateThreadEx)(
-        OUT PHANDLE hThread,
-        IN ACCESS_MASK DesiredAccess,
-        IN PVOID ObjectAttributes,
-        IN HANDLE ProcessHandle,
-        IN PVOID lpStartAddress,
-        IN PVOID lpParameter,
-        IN ULONG Flags,
-        IN SIZE_T StackZeroBits,
-        IN SIZE_T SizeOfStackCommit,
-        IN SIZE_T SizeOfStackReserve,
-        OUT PVOID lpBytesBuffer
-        );
-
-   
+LPVOID lb() {
     LPVOID lb = (LPVOID)GetProcAddress(GetModuleHandleW(L"kernel32.dll"), "LoadLibraryW");
     if (lb == NULL) {
-        if (debuggingMode) {
-            std::cerr << "\033[31m[ERROR] Can't get LoadLibraryW address, error: \033[0m"<< GetLastError() << std::endl;
-        }
-        return -1;
+        log(logLevel::ERRORR, injectionStage::LOAD_LIBRARY_ADDR, "Can't get LoadLibraryW address");
+        return NULL;
     }
+    return lb;
 
+}
+
+
+
+int openProc(int pid) {
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, DWORD(pid));
     if (hProcess == NULL) {
-        if (debuggingMode) {
-            std::cerr << "\033[31m" << currentTime()
-                << " [ERROR] Can't open process, error: \033[0m"<< GetLastError() << std::endl;
-        }
+        log(logLevel::ERRORR, injectionStage::OPEN_PROCESS, "Can't open process");
         return -1;
     }
-    else if (debuggingMode) {
-        std::cerr << "\033[34m" << currentTime() << " [INFO] Process opened successfully\033[0m" << std::endl;
+    else {
+        log(logLevel::INFO, injectionStage::OPEN_PROCESS, "Process opened successfully");
+        alocForProc(hProcess);
+
     }
 
+}
+
+
+
+int alocForProc(HANDLE hProcess) {
 
     LPVOID pRemoteMemory = VirtualAllocEx(hProcess, NULL, dllSiz, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
     if (pRemoteMemory == NULL) {
-        if (debuggingMode) {
-            std::cerr << "\033[31m" << currentTime()<< " [ERROR] Can't allocate memory, error: \033[0m" << GetLastError() << std::endl;
-        }
+        log(logLevel::ERRORR, injectionStage::ALLOC_MEMORY, "Can't allocate memory");
         CloseHandle(hProcess);
         return -1;
     }
-    else if (debuggingMode) {
-        std::cerr << "\033[34m" << currentTime() << " [INFO] Memory allocated\033[0m" << std::endl;
+    else {
+        log(logLevel::INFO, injectionStage::ALLOC_MEMORY, "Memory allocated");
+        writeDLL(hProcess, pRemoteMemory);
     }
+}
 
-  
+
+
+
+
+
+int writeDLL(HANDLE hProcess, LPVOID pRemoteMemory) {
+
     if (!WriteProcessMemory(hProcess, pRemoteMemory, dllPath, dllSiz, NULL)) {
-        if (debuggingMode) {
-            std::cerr << "\033[31m" << currentTime()<< " [ERROR] Can't write memory, error: \033[0m"<< GetLastError() << std::endl;
-        }
+        log(logLevel::ERRORR, injectionStage::WRITE_MEMORY, "Can't write memory");
         VirtualFreeEx(hProcess, pRemoteMemory, 0, MEM_RELEASE);
         CloseHandle(hProcess);
         return -1;
     }
-    else if (debuggingMode) {
-        std::cerr << "\033[34m" << currentTime()<< " [INFO] DLL path written\033[0m" << std::endl;
-    }
 
-    
+    else {
+        log(logLevel::INFO, injectionStage::WRITE_MEMORY, "DLL path written");
+        ntCTE(hProcess, pRemoteMemory);
+    }
+}
+
+
+
+
+
+int ntCTE(HANDLE hProcess, LPVOID pRemoteMemory) {
     HANDLE hThread = NULL;
     NTSTATUS status = 0;
-    bool ntSuccess = true;
-   
+
     HMODULE hNtdll = GetModuleHandleW(L"ntdll.dll");
-    if (hNtdll) {
-        pNtCreateThreadEx NtCTE = (pNtCreateThreadEx)GetProcAddress(hNtdll, "NtCreateThreadEx");
-        if (NtCTE) {
-            status = NtCTE(&hThread,0x1FFFFF,NULL,hProcess,lb,pRemoteMemory,0, 0, 0, 0, NULL);
-
-       
-            if (status >= 0) {  
-                if (debuggingMode) {
-                    std::cerr << "\033[34m" << currentTime()<< " [INFO] NtCreateThreadEx succeeded (status: 0x"<< std::hex << status << ")\033[0m" << std::endl;
-                }
-               
-            }
-            else if (debuggingMode) {
-                    std::cerr << "\033[33m" << currentTime()<< " [WARN] NtCreateThreadEx failed (status: 0x"<< std::hex << status << ")\033[0m" << std::endl;
-                    ntSuccess = false;
-               
-            }
-        }
-        else if (debuggingMode) {
-                std::cerr << "\033[33m" << currentTime()<< " [WARN] Can't find NtCreateThreadEx, error: \033[0m"<< GetLastError() << std::endl;
-                ntSuccess = false;
-           
-        }
+    if (!hNtdll) {
+        log(logLevel::WARN, injectionStage::NTDLL_LOAD, "Can't load ntdll.dll");
+        return ntCTEFall(hProcess, pRemoteMemory, false, NULL);
     }
-    else if (debuggingMode) {
-            std::cerr << "\033[33m" << currentTime()<< " [WARN] Can't load ntdll.dll, error: \033[0m"<< GetLastError() << std::endl;
-            ntSuccess = false;
-    }
-    
 
-   
-    if (UseCreateRemoteThreadAfterNT && !ntSuccess) {
-        hThread = CreateRemoteThread(hProcess,NULL,0,(LPTHREAD_START_ROUTINE)lb,pRemoteMemory,0, NULL);
+    pNtCreateThreadEx NtCTE = (pNtCreateThreadEx)GetProcAddress(hNtdll, "NtCreateThreadEx");
+    if (!NtCTE) {
+        log(logLevel::WARN, injectionStage::NTDLL_FIND, "Can't find NtCreateThreadEx");
+        return ntCTEFall(hProcess, pRemoteMemory, false, NULL);
+    }
+
+    status = NtCTE(&hThread, THREAD_ALL_ACCESS, NULL, hProcess, lb(), pRemoteMemory, 0, 0, 0, 0, NULL);
+
+    if (status >= 0 && hThread) {
+        log(logLevel::INFO, injectionStage::NTDLL_CREATE_THREAD, "NtCreateThreadEx succeeded");
+        WaitForSingleObject(hThread, INFINITE);
+        log(logLevel::SUCCESS, injectionStage::COMPLETE, "Injected!!!");
+        return closeALL(hProcess, pRemoteMemory, hThread);
+    }
+    else {
+        log(logLevel::WARN, injectionStage::NTDLL_CREATE_THREAD, "NtCreateThreadEx failed");
+        return ntCTEFall(hProcess, pRemoteMemory, false, hThread);
+    }
+}
+
+
+
+
+
+
+int ntCTEFall(HANDLE hProcess, LPVOID pRemoteMemory, bool ntSuccess, HANDLE hThread) {
+
+
+
+    if (UseCreateRemoteThreadAfterNT && ntSuccess != NULL) {
+        hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lb(), pRemoteMemory, 0, NULL);
 
         if (hThread == NULL) {
-            if (debuggingMode) {
-                std::cerr << "\033[31m" << currentTime() << " [ERROR] CreateRemoteThread failed, error: \033[0m"<< GetLastError() << std::endl;
-                
-            }
-            
+            log(logLevel::ERRORR, injectionStage::CREATE_REMOTE_THREAD, "CreateRemoteThread failed");
             VirtualFreeEx(hProcess, pRemoteMemory, 0, MEM_RELEASE);
             CloseHandle(hProcess);
             return -1;
         }
 
-        if (debuggingMode) {
-            std::cerr << "\033[34m" << currentTime()<< " [INFO] Using CreateRemoteThread\033[0m" << std::endl;
-        }
+
+
+        log(logLevel::INFO, injectionStage::CREATE_REMOTE_THREAD, "Using CreateRemoteThread");
+
+
+        WaitForSingleObject(hThread, INFINITE);
+
+
+        log(logLevel::SUCCESS, injectionStage::COMPLETE, "Injected!!!");
+        closeALL(hProcess, pRemoteMemory, hThread);
+
+
     }
+}
 
- 
-   /* WaitForSingleObject(hThread, INFINITE);*/
 
-    if (debuggingMode) {
-        std::cerr << "\033[32m" << currentTime()<< " [SUCCESS] Injection completed!\033[0m" << std::endl;
-        std::cin.get();
-    }
 
-    
+
+
+
+
+
+
+int closeALL(HANDLE hProcess, LPVOID pRemoteMemory, HANDLE hThread) {
     CloseHandle(hThread);
     VirtualFreeEx(hProcess, pRemoteMemory, 0, MEM_RELEASE);
     CloseHandle(hProcess);
-
+    std::cin.get();
     return 0;
+}
+
+
+
+
+
+
+int injectorMain(int pid) {
+
+    return openProc(pid);
+
+
 }
--- 1/main.h
+++ 1/main.h
@@ -8,12 +8,67 @@
 
 
 
+
+
+
+//FOR NtCreateThreadEx
+typedef NTSTATUS(NTAPI* pNtCreateThreadEx)(
+    OUT PHANDLE hThread,
+    IN ACCESS_MASK DesiredAccess,
+    IN PVOID ObjectAttributes,
+    IN HANDLE ProcessHandle,
+    IN PVOID lpStartAddress,
+    IN PVOID lpParameter,
+    IN ULONG Flags,
+    IN SIZE_T StackZeroBits,
+    IN SIZE_T SizeOfStackCommit,
+    IN SIZE_T SizeOfStackReserve,
+    OUT PVOID lpBytesBuffer
+    );
+
+
+
+//For log.cpp
+enum class logLevel {
+    INFO,
+    WARN,
+    ERRORR,
+    SUCCESS
+
+};
+
+enum class injectionStage {
+    INIT = 0,
+    FIND_PID,
+    LOAD_LIBRARY_ADDR,
+    OPEN_PROCESS,
+    ALLOC_MEMORY,
+    WRITE_MEMORY,
+    NTDLL_FIND,
+    NTDLL_LOAD,
+    NTDLL_CREATE_THREAD,
+    CREATE_REMOTE_THREAD,
+    CLEANUP,
+    COMPLETE
+};
+
+
+
+
+
+
+void log(logLevel level, injectionStage stage, const std::string& msg);
 int findProcID(const wchar_t* processName);
 int injectorMain(int pid);
 std::string currentTime();
 
 
-
+int openProc(int pid);
+int alocForProc(HANDLE hProcess);
+int writeDLL(HANDLE hProcess, LPVOID pRemoteMemory);
+int ntCTE(HANDLE hProcess, LPVOID pRemoteMemory);
+int ntCTEFall(HANDLE hProcess, LPVOID pRemoteMemory, bool ntSuccess, HANDLE hThread);
+int closeALL(HANDLE hProcess, LPVOID pRemoteMemory, HANDLE hThread);
 
 const bool debuggingMode = true;   //Change to disable error,info,warn(log) information
-const bool UseCreateRemoteThreadAfterNT = true; // Change to disable using CreateRemoteThread if NtCreateThreadEx crash
+const bool UseCreateRemoteThreadAfterNT = true; // Change to disable using CreateRemoteThread if NtCreateThreadEx crash!!!!
